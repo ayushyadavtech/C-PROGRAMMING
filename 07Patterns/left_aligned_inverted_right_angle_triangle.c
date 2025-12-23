c4305c84351076c27390df56928e54f99fa9a93b
AYUSH YADAV <ayushyad176@gmail.com>
2025-12-23 16:54:31 +0530

    Add left-aligned inverted right angle triangle pattern
    This program prints a left-aligned inverted right angle triangle pattern based on user-defined height.

---
+++ 07Patterns/left_aligned_inverted_right_angle_triangle.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main() {
+
+    int num;
+
+    printf("Enter height of pattern\n");
+    scanf("%d", &num);
+
+    for(int i=1; i<=num; i++) {
+
+        for(int j=num; j>=i; j--) {
+            printf("* ");
+        }
+
+        printf("\n");
+    }
+
+
+    return 0;
+}
